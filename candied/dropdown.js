bd80e60f36fccd5a6ebfdf1a2b88a01af90273d3
Nadettt <65888795+Nadettt@users.noreply.github.com>
2020-06-30 14:32:09 +0200

    responsive menu working on mainpg.html
    -created responsive menu with Javascript (mainpg)
    -pages linked and button links working
    -added more pages (custom, basket, checkout)

--- candied/dropdown.js
+++ candied/dropdown.js
@@ -1,3 +1,16 @@
-function myFunction() {
-	document.getElementById('myDropdown').classList.toggle('show');
+//declare element variables - menu and button
+let hamburgerBtn = document.querySelector('#hamburgerBtn');
+let crossBtn = document.querySelector('#crossBtn');
+let dropMenu = document.querySelector('.dropdown');
+
+//event listener
+hamburgerBtn.addEventListener('click', myDropdown);
+crossBtn.addEventListener('click', myDropdown);
+
+//function for dropdown
+function myDropdown() {
+	dropMenu.classList.toggle('hide');
+	dropMenu.classList.toggle('show');
 }
+
+//call the function
