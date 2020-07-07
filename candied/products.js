825ec650fc405c6014ffd4882cca918511001556
Nadettt <65888795+Nadettt@users.noreply.github.com>
2020-07-07 14:05:03 +0200

    cakespg.html button link fixes
    - fixed links for info button on cakes page
    - fixed some styling

--- candied/products.js
+++ candied/products.js
@@ -1,32 +1,30 @@
-const template = document.querySelector("#myTemplate").content;
+const template = document.querySelector('#myTemplate').content;
 
 const productListLink =
-  "https://spreadsheets.google.com/feeds/list/1RE76sRsyktVIq6FLfKUIfNqtGYCq593ubbXJo8DdMlM/od6/public/values?alt=json ";
+	'https://spreadsheets.google.com/feeds/list/1RE76sRsyktVIq6FLfKUIfNqtGYCq593ubbXJo8DdMlM/od6/public/values?alt=json ';
 function loadJSON(link) {
-  fetch(productListLink)
-    .then(e => e.json())
-    .then(data => data.feed.entry.forEach(displayProducts));
+	fetch(productListLink).then((e) => e.json()).then((data) => data.feed.entry.forEach(displayProducts));
 }
 
-
 function displayProducts(products) {
-  const clone = template.cloneNode("true");
-  clone.querySelector("img").setAttribute("src", `img-products/${products.gsx$img.$t}`);
-  clone.querySelector("#category").textContent = products.gsx$category.$t;
-  clone.querySelector("#name").textContent = products.gsx$name.$t;
-  clone.querySelector("#price").textContent = products.gsx$price.$t;
+	const clone = template.cloneNode('true');
+	clone.querySelector('img').setAttribute('src', `img-products/${products.gsx$img.$t}`);
+	clone.querySelector('#category').textContent = products.gsx$category.$t;
+	clone.querySelector('#name').textContent = products.gsx$name.$t;
+	clone.querySelector('#price').textContent = products.gsx$price.$t;
+	clone.querySelector('#link').setAttribute('href', `${products.gsx$link.$t}`);
 
-  if (products.gsx$category.$t === "cakes") {
-    document.querySelector("#cakes").appendChild(clone);
-  } else if (products.gsx$category.$t === "chocolate") {
-    document.querySelector("#chocolate").appendChild(clone);
-  } else if (products.gsx$category.$t === "pies") {
-    document.querySelector("#pies").appendChild(clone);
-  } else if (products.gsx$category.$t === "pastry") {
-    document.querySelector("#pastry").appendChild(clone);
-  } else {
-    document.querySelector("#other").appendChild(clone);
-  }
+	if (products.gsx$category.$t === 'cakes') {
+		document.querySelector('#cakes').appendChild(clone);
+	} else if (products.gsx$category.$t === 'chocolate') {
+		document.querySelector('#chocolate').appendChild(clone);
+	} else if (products.gsx$category.$t === 'pies') {
+		document.querySelector('#pies').appendChild(clone);
+	} else if (products.gsx$category.$t === 'pastry') {
+		document.querySelector('#pastry').appendChild(clone);
+	} else {
+		document.querySelector('#other').appendChild(clone);
+	}
 }
 
 loadJSON(productListLink);
