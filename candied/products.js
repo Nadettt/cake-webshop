32e99b42bb60cc88a7e4a52121797c86c43e7469
Nadettt <65888795+Nadettt@users.noreply.github.com>
2020-07-08 13:43:25 +0200

    code fix snackbar
    - button is created dynamically, therefore I had to move the code to the product.js file (where the button is generated) and delete the initially created individual js file of the snackbar

--- candied/products.js
+++ candied/products.js
@@ -1,4 +1,7 @@
 const template = document.querySelector('#myTemplate').content;
+let snackbar = document.querySelector('#snackbarCakes');
+let btnBasket = document.querySelector('.btnBasket');
+let popup = document.querySelector('.snack1');
 
 const productListLink =
 	'https://spreadsheets.google.com/feeds/list/1RE76sRsyktVIq6FLfKUIfNqtGYCq593ubbXJo8DdMlM/od6/public/values?alt=json ';
@@ -14,16 +17,23 @@ function displayProducts(products) {
 	clone.querySelector('#price').textContent = products.gsx$price.$t;
 	clone.querySelector('#link').setAttribute('href', `${products.gsx$link.$t}`);
 
+	clone.querySelector('button').addEventListener('click', () => {
+		popup.classList.add('show');
+		/*setTimeout(function() {
+			popup.snackbar = popup.snackbar.replace('show', '');
+		}, 3000);*/
+	});
+
 	if (products.gsx$category.$t === 'cakes') {
 		document.querySelector('#cakes').appendChild(clone);
-	} else if (products.gsx$category.$t === 'chocolate') {
+		/* }else if (products.gsx$category.$t === 'chocolate') {
 		document.querySelector('#chocolate').appendChild(clone);
 	} else if (products.gsx$category.$t === 'pies') {
 		document.querySelector('#pies').appendChild(clone);
 	} else if (products.gsx$category.$t === 'pastry') {
 		document.querySelector('#pastry').appendChild(clone);
 	} else {
-		document.querySelector('#other').appendChild(clone);
+		document.querySelector('#other').appendChild(clone);*/
 	}
 }
 
