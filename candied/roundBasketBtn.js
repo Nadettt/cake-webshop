fd6abe0d5d386d088907cdc6d730f2b9ff9e4a90
Nadettt <65888795+Nadettt@users.noreply.github.com>
2020-07-28 18:00:35 +0200

    Design changes

---
+++ candied/roundBasketBtn.js
@@ -0,0 +1,12 @@
+const div = document.querySelector('.container-custom').content;
+let snackbar = document.querySelector('#snackbarCakes');
+let basketRound = document.querySelector('#basketRound');
+let popup = document.querySelector('.snack1');
+
+function myFunction() {
+	// Get the snackbar DIV
+	var snack1 = document.getElementById('snackbar');
+
+	// Add the "show" class to DIV
+	x.className = 'show';
+}
